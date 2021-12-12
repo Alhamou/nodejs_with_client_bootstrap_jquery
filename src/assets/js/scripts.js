0073b77a4a2971bcbed5d493b042e94f979630ae
Emad Alhamou <emadalhamo@gmail.com>
2021-12-12 03:21:15 +0100

    first

---
+++ src/assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+    })
+
+
+})
